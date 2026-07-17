5321f3d9b1b883f703127c4740b65eef74a24fcf
00nkm <66212548+00nkm@users.noreply.github.com>
2026-07-17 18:41:03 -0300

    Change database engine to use NullPool
    Updated database connection to use NullPool for engine.

--- services/database.py
+++ services/database.py
@@ -1,9 +1,11 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/postgres')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
