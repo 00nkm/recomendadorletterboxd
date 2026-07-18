2f6ce86f6a10d921c24e1e7e3b7b6c047ea8b13e
00nkm <66212548+00nkm@users.noreply.github.com>
2026-07-18 20:35:07 -0300

    Increase height of header rectangle in SVG

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,12 +33,11 @@ export default function Header({ currentPage, onNavigate }: HeaderProps) {
           onClick={() => onNavigate('home')}
         >
           <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <rect x="2" y="3" width="20" height="14" rx="2" />
+            <rect x="2" y="3" width="20" height="18" rx="2" />
             <path d="M8 21h8M12 17v4" />
           </svg>
           Descobrir
         </NavButton>
-
         <NavButton
           active={currentPage === 'us'}
           onClick={() => onNavigate('us')}
